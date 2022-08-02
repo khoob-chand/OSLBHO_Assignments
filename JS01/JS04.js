ac084930d78fcd16542b0fadde39da445d9741b0
khoobchand <khoobchandjhariya48@gmail.com>
2022-08-02 11:13:28 +0530

    JS01 Added Assignment

---
+++ JS01/JS04.js
@@ -0,0 +1,4 @@
+var car={name:"Audi",color:"black",model:"PS3"};
+for(const property in car){
+    console.log(`${property}=${car[property]}`);
+}
---
+++ JS01/JS05.js
@@ -0,0 +1,4 @@
+var fruits = ["Apple", "Banana", "Carrot", "Guava", "Orange"];
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
